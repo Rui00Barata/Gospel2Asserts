1d7617d21cd3cae8b36741329c7495a86b8d9910
Rui00Barata <rui.barata@ubi.pt>
2023-07-19 16:25:33 +0100

    Started pattern-matching translation

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,8 @@
 module G = Gospel
 open G.Uast
 
+exception Not_Supported
+
 let fname = ref None
 
 let usage_msg = "toy_gospel <file>.ml"
@@ -35,8 +37,14 @@ let gospel_ast =
    
 let rec term_to_expression t = 
   match t.term_desc with
-  | Ttrue                                   -> assert false
-  | Tfalse                                  -> assert false
+  | Ttrue                                   -> 
+      let f = Longident.Lident "true" in
+      let f_sed = Sexp_construct (Location.mkloc f t.term_loc, None) in
+      {spexp_desc = f_sed; spexp_loc = t.term_loc; spexp_loc_stack = []; spexp_attributes = []}
+  | Tfalse                                  -> 
+      let f = Longident.Lident "false" in
+      let f_sed = Sexp_construct (Location.mkloc f t.term_loc, None) in
+      {spexp_desc = f_sed; spexp_loc = t.term_loc; spexp_loc_stack = []; spexp_attributes = []}
   | Tconst c                                -> 
       let sed_c = Sexp_constant c in
       {spexp_desc = sed_c; spexp_loc = t.term_loc; spexp_loc_stack = []; spexp_attributes = []}
@@ -46,39 +54,41 @@ let rec term_to_expression t =
       let v = Longident.Lident (qp.pid_str)  in
       let sed_v = Sexp_ident (Location.mkloc v qp.pid_loc) in
       {spexp_desc = sed_v; spexp_loc = t.term_loc; spexp_loc_stack = []; spexp_attributes = []}
-    | Qdot (_,qp) ->
-      let v = Longident.Lident (qp.pid_str)  in
-      let sed_v = Sexp_ident (Location.mkloc v qp.pid_loc) in
-      {spexp_desc = sed_v; spexp_loc = t.term_loc; spexp_loc_stack = []; spexp_attributes = []})
-  | Tidapp _                                -> assert false
-  | Tfield _                                -> assert false
-  | Tapply _                                -> assert false
+    | Qdot _    -> raise Not_Supported)
+  | Tidapp _                                -> raise Not_Supported
+  | Tfield _                                -> raise Not_Supported
+  | Tapply _                                -> raise Not_Supported
   | Tinfix (t1, pid, t2)                    -> 
-    let e2 = term_to_expression t1 in
     let e3 = term_to_expression t2 in
+    let e2 = term_to_expression t1 in
     let op = Longident.Lident (List.nth (String.split_on_char ' ' pid.pid_str) 1) in
     let sed_op = Sexp_ident (Location.mkloc op pid.pid_loc) in
     let e1 = {spexp_desc = sed_op; spexp_loc = pid.pid_loc; spexp_loc_stack = []; spexp_attributes = []} in
     {spexp_desc = Sexp_apply (e1, [(Nolabel, e2); (Nolabel, e3)]); spexp_loc = t.term_loc; spexp_loc_stack = []; spexp_attributes = []}
-  | Tbinop _                                -> assert false
-  | Tnot _                                  -> assert false
-  | Tif _                                   -> assert false
-  | Tquant _                                -> assert false
-  | Tattr _                                 -> assert false
-  | Tlet _                                  -> assert false
-  | Tcase _                                 -> assert false
-  | Tcast _                                 -> assert false
-  | Ttuple _                                -> assert false
-  | Trecord _                               -> assert false
-  | Tupdate _                               -> assert false
-  | Tscope _                                -> assert false
+  | Tbinop _                                -> raise Not_Supported
+  | Tnot t1                                 -> 
+    let t_sexp  = term_to_expression t1 in
+    let n       = Longident.Lident "not" in
+    let n_sed   = Sexp_ident (Location.mkloc n t.term_loc) in
+    let e1      = {spexp_desc = n_sed; spexp_loc = t.term_loc; spexp_loc_stack = []; spexp_attributes = []} in
+    {spexp_desc = Sexp_apply (e1, [(Nolabel, t_sexp)]); spexp_loc = {loc_start = t.term_loc.loc_start; loc_end = t_sexp.spexp_loc.loc_end; loc_ghost = t.term_loc.loc_ghost}; spexp_loc_stack = []; spexp_attributes = []}
+  | Tif _                                   -> raise Not_Supported
+  | Tquant _                                -> raise Not_Supported
+  | Tattr _                                 -> raise Not_Supported
+  | Tlet _                                  -> raise Not_Supported
+  | Tcase _                                 -> raise Not_Supported
+  | Tcast _                                 -> raise Not_Supported
+  | Ttuple _                                -> raise Not_Supported
+  | Trecord _                               -> raise Not_Supported
+  | Tupdate _                               -> raise Not_Supported
+  | Tscope _                                -> raise Not_Supported
   | Told _                                  -> 
       (* tenho que adicionar uma variavel ao inicio para guardar o valor inicial de term *)
-      assert false
+      raise Not_Supported
 
 let updated_expression exp pre post =
-  let _ = List.map term_to_expression pre in
-  let _ = List.map term_to_expression post in
+  let _ (* pre *)   = List.map term_to_expression pre in
+  let _ (* post *)  = List.map term_to_expression post in
   exp
 
 
